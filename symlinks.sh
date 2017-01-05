976d5821ffa73eba02ab951eb8a6bca5e7551661
ashleyf <ashley.feniello@live.com>
2017-01-04 22:01:18 -0800

    symlink script

---
+++ symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s .bashrc ~/.bashrc
+ln -s .emacs ~/.emacs
+ln -s .zshrc ~/.zshrc
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .vimrc ~/.vimrc
+ln -s .zshrc ~/.zshrc
